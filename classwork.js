66906dbfe861f382a0dd21a130758c59dbbacc85
salmunefi <85169532+salmunefi@users.noreply.github.com>
2022-06-29 18:07:09 +0300

    Class Work 4 Part 1

---
+++ classwork.js
@@ -0,0 +1,7 @@
+alert ('Welcome to My Wesbite, My name is Salman Al Munefi and I am gonna calculate your grade')
+
+let age = prompt('Enter Your Grade')
+alert('Grade')
+
+console.log(age)
+console.log('Grade')
