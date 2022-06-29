caf49a97a54ba19bdda480ebdc4094ac8ee30758
salmunefi <85169532+salmunefi@users.noreply.github.com>
2022-06-29 19:14:03 +0300

    Classwork 4 Part 2

--- classwork.js
+++ classwork.js
@@ -1,8 +1,12 @@
-alert ('Welcome to My Wesbite, My name is Salman Al Munefi and I am gonna calculate your grade')
+alert('Welcome to My Wesbite, My name is Salman Al Munefi and I am gonna calculate your grade')
+let grade = prompt('Enter Your Grade')
 
-let age = prompt('Enter Your Grade')
-alert('Grade')
-
+if (grade>=90 && grade<= 100) { alert('you got A')}
+if (grade>=80 && grade<= 89) { alert('you got B')}
+if (grade>=70 && grade<= 79) { alert('you got C')}
+if (grade>=60 && grade<= 69) { alert('you got D+')}
+if (grade>=50 && grade<= 59) { alert('you got D')}
+if (grade<=49) { alert ('You Failed The Course')}
 console.log(age)
 
 console.log("%cGrade", "color: orange");
@@ -12,3 +16,5 @@ let baseStyles = [
     "padding: 2px 4px",
     "border-radius: 2px"
   ].join(";");
+
+// javascript is not java
